ec82478c06fe2e2e84eacc5dbc6f3494cf7fa47d
al-ali <raed_alali@me.com>
2017-03-25 18:01:05 -0400

    Add project files.

---
+++ firat program/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firat_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is the first program
+            Console.WriteLine("hello world");
+            Console.ReadKey();
+        }
+    }
+}
